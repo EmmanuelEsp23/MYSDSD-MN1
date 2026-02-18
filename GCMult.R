026e54836a7ac109c03d85c76f3e69a3809ab487
EmmanuelEsp23 <emmanuel.gerardo52@gmail.com>
2026-02-18 11:44:29 -0600

    Generador congruencial multiplicativo gg papa

--- GCMult.R
+++ GCMult.R
@@ -14,3 +14,34 @@ seeds <- numeric(pe) #Esta madre se puede hacer con un array, pero con la funci�
 seeds[1] <- seed # Guardamos la semilla inicial en el primer lugar del vector
 count <- 1
 repetido <- FALSE
+
+while (!repetido) {
+    #Formula del generador congruencial multiplicativo
+    xn <- (a * seeds[count]) %% m
+
+    # Verificamos si ya existe en nuestro vector (no solo la semilla)
+    # O si ya regresó a la semilla inicial
+    if (xn == seed || count >= pe) {
+        repetido <- TRUE
+    } else {
+        count <- count + 1
+        seeds[count] <- xn
+    }
+}
+
+# Limpiamos el vector de los ceros sobrantes
+resultado <- seeds[1:count]
+cat("\nCantidad de números generados:", length(resultado), "\n")
+
+if (length(resultado) == pe) { #condicional de período de fiabilidad
+    cat("El generador es de periodo completo\n")
+} else {
+    cat("El generador es de periodo incompleto\n")
+}
+
+#guardar los números rectangulares en un CSV
+numeros <- resultado / m # Convertimos a números rectangulares
+format <- format(round(numeros, 5), nsmall = 5) # Formateamos los números a 5 decimales
+df <- data.frame(NumeroRectangular = format) # Creamos un dataframe para guardar los números
+write.csv(df, file = paste0("datasetsGCMult/", readline(prompt = "\nNombre del archivo: "), ".csv"), row.names = FALSE) # Guardamos el dataframe en un CSV
+cat("Archivo generado.\n")
